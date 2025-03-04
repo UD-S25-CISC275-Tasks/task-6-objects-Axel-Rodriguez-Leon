6e5057988194ef0aca460d38f5b422a7780ba9e3
Axel Rodriguez-Leon <axelarl@udel.edu>
2025-03-03 22:38:19 -0500

    Finished toShortForm

--- src/objects.ts
+++ src/objects.ts
@@ -42,7 +42,12 @@ export function isCorrect(question: Question, answer: string): boolean {
  * be exactly one of the options.
  */
 export function isValid(question: Question, answer: string): boolean {
-    return false;
+    const valid = (question: Question, answer: string): boolean => {
+        return question.type === "multiple_choice_question" ?
+                question.options.includes(answer)
+            :   true;
+    };
+    return valid(question, answer);
 }
 
 /**
@@ -52,7 +57,7 @@ export function isValid(question: Question, answer: string): boolean {
  * name "My First Question" would become "9: My First Q".
  */
 export function toShortForm(question: Question): string {
-    return "";
+    return `${question.id}: ${question.name.slice(0, 10)}`;
 }
 
 /**
